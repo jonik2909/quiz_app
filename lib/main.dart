3fe5d0029f5ea582a6052d1c805bec14b740e1c5
jonik2909 <boronovjonibek@gmail.com>
2023-10-26 12:47:17 +0900

    feat: BRR: initialize setup

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 99, 22, 166),
+                Color.fromARGB(255, 53, 13, 104),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
