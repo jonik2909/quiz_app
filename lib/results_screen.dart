0fc6a56bc558e4cf6cde053791fefe8c092884c1
jonik2909 <boronovjonibek@gmail.com>
2023-10-30 18:42:22 +0900

    feat: exdpanded rescue

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -13,7 +14,7 @@ class ResultsScreen extends StatelessWidget {
       summary.add({
         'question_index': i,
         'question': questions[i].text,
-        'correc_answer': questions[i].answers[0],
+        'correct_answer': questions[i].answers[0],
         'user_answer': chosenAnswers[i],
       });
     }
@@ -23,6 +24,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -31,9 +38,11 @@ class ResultsScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             // crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Text('You answered X out of Y questions correctly'),
+              Text(
+                'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly',
+              ),
               const SizedBox(height: 30),
-              const Text('List of answers and question...'),
+              QuestionsSummary(getSummaryData()),
               const SizedBox(height: 30),
               TextButton(
                 onPressed: () {},
